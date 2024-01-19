8b700fb0cb6e089e8a8bdfe9fa189329f52d384a
Jack <jackthagamerhd@gmail.com>
2024-01-18 19:19:49 -0700

    Output range of two numbers, roof not checked for being greater than or equal to the flow and can be positive or negative, inability to parse an integer isn't handled

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char sep = '-';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("%f%c%f", atof(argv[1]), sep, atof(argv[1 + 1]));
+
 	return 0;
 }
